a8eb792639c2049b5ff6af99dfdf17bfc24bd70c
Sean Liao <sean+git@liao.dev>
2022-07-01 18:57:53 +0100

    report other views

--- main.go
+++ main.go
@@ -12,8 +12,9 @@ import (
 )
 
 func main() {
-	var fp string
+	var fp, view string
 	flag.StringVar(&fp, "f", "fin.cue", "data file")
+	flag.StringVar(&view, "view", "holding", "holding | income | expense")
 	flag.Parse()
 
 	b, err := os.ReadFile(fp)
@@ -33,22 +34,34 @@ func main() {
 	tw := tabwriter.NewWriter(os.Stdout, 1, 8, 2, ' ', tabwriter.AlignRight)
 
 	for _, currency := range out {
+		var titles []string
+		switch view {
+		case "holding":
+			titles = currency.Holding
+		case "income":
+			titles = currency.Income
+		case "expense":
+			titles = currency.Expense
+		default:
+			log.Fatalln("unknown view", view)
+		}
+
 		fmt.Fprintln(tw, "currency:", currency.Currency)
-		bag := make(map[string]int)
 		fmt.Fprint(tw, "---\t")
-		for _, holding := range currency.Holding {
-			fmt.Fprint(tw, holding, "\t")
+		for _, title := range titles {
+			fmt.Fprint(tw, title, "\t")
 		}
 		fmt.Fprintln(tw)
 
+		bag := make(map[string]int)
 		for _, month := range currency.Months {
 			fmt.Fprint(tw, month.Year, "-", month.Month, "\t")
 			for _, transaction := range month.Transactions {
 				bag[transaction.Source] -= transaction.Amount
 				bag[transaction.Destination] += transaction.Amount
 			}
-			for _, holding := range currency.Holding {
-				fmt.Fprintf(tw, "%.2f\t", float64(bag[holding])/100)
+			for _, title := range titles {
+				fmt.Fprintf(tw, "%.2f\t", float64(bag[title])/100)
 			}
 			fmt.Fprintln(tw)
 		}
