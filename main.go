eef6de57f74b0a7e72e95f6d6f7bf6fe1ac83c43
Sean Liao <sean+git@liao.dev>
2023-08-24 23:21:00 +0100

    refactor

--- main.go
+++ main.go
@@ -3,30 +3,90 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"log/slog"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/google/subcommands"
-	"go.seankhliao.com/fin/v3/subcommands/serve"
-	"go.seankhliao.com/fin/v3/subcommands/submit"
-	"go.seankhliao.com/fin/v3/subcommands/view"
+	"go.seankhliao.com/svcrunner/v2/observability"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 func main() {
-	name := "fin"
-	fset := flag.NewFlagSet(name, flag.ExitOnError)
-	cmdr := subcommands.NewCommander(fset, name)
-	cmdr.Register(&serve.Cmd{}, "server")
+	conf := &Config{}
 
-	cmdr.Register(&view.Cmd{}, "client")
-	cmdr.Register(&submit.Cmd{}, "client")
+	// flags
+	fset := flag.NewFlagSet("ghdefaults", flag.ExitOnError)
+	conf.SetFlags(fset)
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "missing subcommand view|submit|serve")
+		os.Exit(1)
+	}
+	fset.Parse(os.Args[2:])
 
-	cmdr.Register(cmdr.HelpCommand(), "other")
+	// observability
+	o := observability.New(conf.o)
 
-	fset.Parse(os.Args[1:])
-
+	// context
 	ctx := context.Background()
-	ctx, _ = signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
-	os.Exit(int(cmdr.Execute(ctx)))
+	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	var err error
+	switch os.Args[1] {
+	case "view":
+		err = View(ctx, o, conf)
+	case "submit":
+		err = Submit(ctx, o, conf)
+	case "serve":
+		err = run(ctx, o, conf)
+	default:
+		fmt.Fprintln(os.Stderr, "unknown subcommand view|submit|serve:", os.Args[1])
+		os.Exit(1)
+	}
+	if err != nil {
+		o.Err(ctx, "exit", err)
+	}
+}
+
+func run(ctx context.Context, o *observability.O, conf *Config) error {
+	mux := http.NewServeMux()
+	h2svr := &http2.Server{}
+	svr := &http.Server{
+		Handler:  h2c.NewHandler(mux, h2svr),
+		ErrorLog: slog.NewLogLogger(o.H, slog.LevelWarn),
+	}
+
+	app := New(ctx, o, conf)
+	app.Register(mux)
+
+	addr := conf.address
+	o.L.LogAttrs(ctx, slog.LevelInfo, "starting listener", slog.String("address", addr))
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		return o.Err(ctx, "failed to listen", err, slog.String("address", addr))
+	}
+
+	go func() {
+		<-ctx.Done()
+		o.L.LogAttrs(ctx, slog.LevelInfo, "starting shutdown", slog.String("reason", context.Cause(ctx).Error()))
+		err := lis.Close()
+		if err != nil {
+			o.Err(ctx, "failed to close listener", err, slog.String("address", addr))
+		}
+		err = svr.Shutdown(context.Background())
+		if err != nil {
+			o.Err(ctx, "failed to close server", err, slog.String("address", addr))
+		}
+	}()
+
+	err = svr.Serve(lis)
+	if err != nil {
+		return o.Err(ctx, "failed to serve", err)
+	}
+	return nil
 }
