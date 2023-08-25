f770369547103e6658876f902c0a4b2c0b1e6d82
Sean Liao <sean+git@liao.dev>
2023-08-25 23:11:48 +0100

    refresh

--- go.mod
+++ go.mod
@@ -1,13 +1,12 @@
 module go.seankhliao.com/fin/v4
 
-go 1.21
+go 1.21.0
 
 require (
 	cuelang.org/go v0.4.3
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.42.0
-	go.seankhliao.com/svcrunner/v2 v2.0.0-20230624155321-19bdf2839035
+	go.seankhliao.com/svcrunner/v3 v3.0.0-20230825210239-4f5a0684e6c9
 	go.seankhliao.com/webstyle v0.0.0-20230625120550-9571efcb99d4
-	golang.org/x/net v0.9.0
 )
 
 require (
@@ -18,8 +17,8 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.2 // indirect
-	github.com/kr/pretty v0.3.1 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.2 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/mpvl/unique v0.0.0-20150818121801-cbe035fff7de // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rogpeppe/go-internal v1.10.0 // indirect
@@ -34,13 +33,13 @@ require (
 	go.opentelemetry.io/otel/sdk v1.16.0 // indirect
 	go.opentelemetry.io/otel/sdk/metric v0.39.0 // indirect
 	go.opentelemetry.io/otel/trace v1.16.0 // indirect
-	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
-	google.golang.org/genproto v0.0.0-20230526203410-71b5a4ffd15e // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20230526203410-71b5a4ffd15e // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230526203410-71b5a4ffd15e // indirect
-	google.golang.org/grpc v1.55.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	go.opentelemetry.io/proto/otlp v0.20.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/grpc v1.57.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,92 +1,235 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"log/slog"
-	"net"
 	"net/http"
+	"net/url"
 	"os"
-	"os/signal"
-	"syscall"
+	"path/filepath"
+	"strings"
 
-	"go.seankhliao.com/svcrunner/v2/observability"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.seankhliao.com/fin/v4/findata"
+	"go.seankhliao.com/svcrunner/v3/framework"
+	"go.seankhliao.com/svcrunner/v3/observability"
+	"go.seankhliao.com/webstyle"
 )
 
 func main() {
 	conf := &Config{}
+	framework.Run(framework.Config{
+		RegisterFlags: conf.SetFlags,
+		Start: func(ctx context.Context, o *observability.O, m *http.ServeMux) (func(), error) {
+			switch conf.mode {
+			case "view":
+				return nil, View(ctx, o, conf)
+			case "submit":
+				return nil, Submit(ctx, o, conf)
+			case "serve":
+				app := New(ctx, o, conf)
+				app.Register(m)
+				return nil, nil
+			}
+			return nil, fmt.Errorf("unknown mode: %q", conf.mode)
+		},
+	})
+}
 
-	// flags
-	fset := flag.NewFlagSet("fin", flag.ExitOnError)
-	conf.SetFlags(fset)
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "missing subcommand view|submit|serve")
-		os.Exit(1)
+type Config struct {
+	mode string
+
+	files         [][]byte
+	submitAddress *url.URL
+	view          findata.View
+
+	dir string
+}
+
+func (c *Config) SetFlags(fset *flag.FlagSet) {
+	fset.StringVar(&c.mode, "mode", "serve", "view|submit|serve")
+
+	fset.Func("file", "files to view/submit", func(s string) error {
+		b, err := os.ReadFile(s)
+		if err != nil {
+			return err
+		}
+		c.files = append(c.files, b)
+		return nil
+	})
+	c.submitAddress, _ = url.Parse("http://fin-ihwa.badger-altered.ts.net/")
+	fset.Func("submit.addr", "http://fin-ihwa.badger-altered.ts.net/", func(s string) error {
+		u, err := url.Parse(s)
+		if err != nil {
+			return err
+		}
+		c.submitAddress = u
+		return nil
+	})
+	fset.Func("view", "holdings|incomes|expenses", func(s string) error {
+		switch s {
+		case "holdings":
+			c.view = findata.ViewHoldings
+		case "incomes":
+			c.view = findata.ViewIncomes
+		case "expenses":
+			c.view = findata.ViewExpenses
+		default:
+			return errors.New("unknown view")
+		}
+		return nil
+	})
+
+	fset.StringVar(&c.dir, "data.dir", "", "data storage dir")
+}
+
+func Submit(ctx context.Context, o *observability.O, conf *Config) error {
+	client := &http.Client{Transport: otelhttp.NewTransport(http.DefaultTransport)}
+	for _, b := range conf.files {
+		cur, err := findata.DecodeOne(b)
+		if err != nil {
+			return o.Err(ctx, "parse file", err)
+		}
+		uc := conf.submitAddress
+		addr := uc.JoinPath(cur.Currency).String()
+		res, err := client.Post(addr, "application/cue", bytes.NewReader(b))
+		if err != nil {
+			return o.Err(ctx, "post file", err, slog.String("addr", addr))
+		} else if res.StatusCode != http.StatusOK {
+			return o.Err(ctx, "post file response", errors.New(res.Status))
+		}
+		o.L.LogAttrs(ctx, slog.LevelInfo, "submitted file")
 	}
-	fset.Parse(os.Args[2:])
+	return nil
+}
 
-	// observability
-	o := observability.New(conf.o)
+func View(ctx context.Context, o *observability.O, conf *Config) error {
+	for _, b := range conf.files {
+		out, err := findata.DecodeOne(b)
+		if err != nil {
+			return o.Err(ctx, "decode file", err)
+		}
 
-	// context
-	ctx := context.Background()
-	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	var err error
-	switch os.Args[1] {
-	case "view":
-		err = View(ctx, o, conf)
-	case "submit":
-		err = Submit(ctx, o, conf)
-	case "serve":
-		err = run(ctx, o, conf)
-	default:
-		fmt.Fprintln(os.Stderr, "unknown subcommand view|submit|serve:", os.Args[1])
-		os.Exit(1)
+		b = out.TabTable(conf.view)
+		fmt.Println(string(b))
 	}
-	if err != nil {
-		o.Err(ctx, "exit", err)
+	return nil
+}
+
+type App struct {
+	o      *observability.O
+	render webstyle.Renderer
+	dir    string
+}
+
+func New(ctx context.Context, o *observability.O, conf *Config) *App {
+	return &App{
+		o:      o,
+		render: webstyle.NewRenderer(webstyle.TemplateCompact),
+		dir:    conf.dir,
 	}
 }
 
-func run(ctx context.Context, o *observability.O, conf *Config) error {
-	mux := http.NewServeMux()
-	h2svr := &http2.Server{}
-	svr := &http.Server{
-		Handler:  h2c.NewHandler(mux, h2svr),
-		ErrorLog: slog.NewLogLogger(o.H, slog.LevelWarn),
+func (a *App) Register(mux *http.ServeMux) {
+	mux.Handle("/eur", otelhttp.NewHandler(a.hView("eur"), "hView - eur"))
+	mux.Handle("/gbp", otelhttp.NewHandler(a.hView("gbp"), "hView - gbp"))
+	mux.Handle("/twd", otelhttp.NewHandler(a.hView("twd"), "hView - twd"))
+	mux.Handle("/", otelhttp.NewHandler(http.HandlerFunc(a.hIndex), "hIndex"))
+	mux.HandleFunc("/-/ready", func(rw http.ResponseWriter, r *http.Request) { rw.Write([]byte("ok")) })
+}
+
+func (a *App) hIndex(rw http.ResponseWriter, r *http.Request) {
+	ctx, span := a.o.T.Start(r.Context(), "hIndex")
+	defer span.End()
+
+	if r.URL.Path != "/" {
+		http.Redirect(rw, r, "/", http.StatusFound)
+		return
 	}
 
-	app := New(ctx, o, conf)
-	app.Register(mux)
+	c := `
+# fin
 
-	addr := conf.address
-	o.L.LogAttrs(ctx, slog.LevelInfo, "starting listener", slog.String("address", addr))
-	lis, err := net.Listen("tcp", addr)
+## money
+
+### _fin_
+
+- [GBP](/gbp)
+- [EUR](/eur)
+- [TWD](/twd)
+`
+
+	err := a.render.Render(rw, strings.NewReader(c), webstyle.Data{})
 	if err != nil {
-		return o.Err(ctx, "failed to listen", err, slog.String("address", addr))
+		a.o.HTTPErr(ctx, "render", err, rw, http.StatusInternalServerError)
 	}
+}
 
-	go func() {
-		<-ctx.Done()
-		o.L.LogAttrs(ctx, slog.LevelInfo, "starting shutdown", slog.String("reason", context.Cause(ctx).Error()))
-		err := lis.Close()
-		if err != nil {
-			o.Err(ctx, "failed to close listener", err, slog.String("address", addr))
-		}
-		err = svr.Shutdown(context.Background())
-		if err != nil {
-			o.Err(ctx, "failed to close server", err, slog.String("address", addr))
-		}
-	}()
+func (a *App) hView(cur string) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		ctx, span := a.o.T.Start(r.Context(), "hView")
+		defer span.End()
 
-	err = svr.Serve(lis)
-	if err != nil {
-		return o.Err(ctx, "failed to serve", err)
-	}
-	return nil
+		var out findata.Currency
+		switch r.Method {
+		case http.MethodPost:
+			b, err := io.ReadAll(r.Body)
+			if err != nil {
+				a.o.HTTPErr(ctx, "read request", err, rw, http.StatusBadRequest)
+				return
+			}
+			out, err = findata.DecodeOne(b)
+			if err != nil {
+				a.o.HTTPErr(ctx, "decode data", err, rw, http.StatusBadRequest)
+				return
+			}
+
+			err = os.WriteFile(filepath.Join(a.dir, cur+".cue"), b, 0o644)
+			if err != nil {
+				a.o.HTTPErr(ctx, "save data", err, rw, http.StatusInternalServerError)
+				return
+			}
+
+		case http.MethodGet:
+			b, err := os.ReadFile(filepath.Join(a.dir, cur+".cue"))
+			if err != nil {
+				a.o.HTTPErr(ctx, "read data file", err, rw, http.StatusInternalServerError)
+				return
+			}
+			out, err = findata.DecodeOne(b)
+			if err != nil {
+				a.o.HTTPErr(ctx, "decode data", err, rw, http.StatusNotFound)
+				return
+			}
+
+		default:
+			a.o.HTTPErr(ctx, "GET or POST", errors.New("bad method"), rw, http.StatusMethodNotAllowed)
+			return
+		}
+
+		var buf bytes.Buffer
+		buf.WriteString("# ")
+		buf.WriteString(cur)
+		buf.WriteString("\n\n## currency view\n\n### _")
+		buf.WriteString(cur)
+		buf.WriteString("_\n\n")
+
+		buf.WriteString("\n#### _holdings_\n\n")
+		buf.Write(out.MarkdownTable(findata.ViewHoldings))
+		buf.WriteString("\n#### _expenses_\n\n")
+		buf.Write(out.MarkdownTable(findata.ViewExpenses))
+		buf.WriteString("\n#### _income_\n\n")
+		buf.Write(out.MarkdownTable(findata.ViewIncomes))
+
+		err := a.render.Render(rw, &buf, webstyle.Data{})
+		if err != nil {
+			a.o.HTTPErr(ctx, "render", err, rw, http.StatusInternalServerError)
+			return
+		}
+	})
 }
